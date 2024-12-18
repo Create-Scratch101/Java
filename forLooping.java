db912d1cf33ed222ce6b0ebd1d7c1516c784709d
Create-Scratch101 <duncandcarr@icloud.com>
2024-12-18 14:49:00 -0500

    Add files via upload

---
+++ forLooping.java
@@ -0,0 +1,9 @@
+public class forLooping{
+  public static void main(String[] e){
+    System.out.print("pre loop");
+    for (int counter = 1;counter < 4;counter++){
+        System.out.print("\nhi");
+    }//for
+    System.out.print("\nafter loop");
+  }//main
+}//class forLooping
